97911b57ba3b635eecf7935b01edeed8f6cb3883
Charles Mwanza <charlesmwanza93@gmail.com>
2015-04-04 05:01:16 +0200

    Intial Commit

---
+++ foobar/assets/js/spa.js
@@ -0,0 +1,17 @@
+ angular.module('Syp', []);
+ angular.module('Syp').controller('BaseCtrl', ['$scope', function ($scope){
+        
+        io.socket.get('/emoji', function (data){
+            $scope.emojis = data;
+            $scope.$apply();
+        });
+        io.socket.on('emoji', function (event){
+            switch (event.verb) {
+             case 'created' :
+                $scope.emojis.push(event.data);
+                $scope.apply();
+                break;
+            }
+        })
+    
+    }]);
